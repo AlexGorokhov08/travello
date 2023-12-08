410bddd3f00cd5c3e787bd4ac017bc582a7014e9
AlexGorokhov08 <Alexgorohov89@gmail.com>
2023-12-08 14:01:58 +0400

    first push

---
+++ app.js
@@ -0,0 +1,58 @@
+const btnMenu = document.querySelector(".menu-btn");
+const navList = document.querySelector(".nav-list");
+const btnClose = document.querySelector(".close-btn");
+
+btnClose.addEventListener("click", () => {
+  navList.classList.toggle("show");
+});
+
+btnMenu.addEventListener("click", () => {
+  navList.classList.toggle("show");
+});
+
+
+/* const viewAll = document.querySelector(".view-all")
+const tabs = document.querySelectorAll(".tabs")
+
+viewAll.addEventListener("click", () => {
+  tabs.classList.toggle("tabs-active")
+}) */
+
+const tabButtons = document.querySelector(".tab-buttons");
+const tabContainer = document.querySelector(".all-tabs");
+
+tabButtons.addEventListener("click", (e) => {
+  const btn = e.target.closest(".tab-btn");
+
+  if (!btn || btn.classList.contains("tab-btn-active")) return;
+
+  const activeBtn = tabButtons.querySelector(".tab-btn-active");
+
+  if (activeBtn) {
+    activeBtn.classList.remove("tab-btn-active");
+  }
+  btn.classList.add("tab-btn-active");
+
+  const activeTab = tabContainer.querySelector(".tabs-active");
+
+  if (activeTab) {
+    activeTab.classList.remove("tabs-active");
+  }
+
+  const tabId = btn.dataset.tab;
+  const selectedTab = tabContainer.querySelector(`[data-tab="${tabId}"]`);
+
+  selectedTab.classList.add("tabs-active");
+});
+
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+});
